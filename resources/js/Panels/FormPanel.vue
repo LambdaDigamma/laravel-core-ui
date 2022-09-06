9b54f6c873bdaea6821dec2be0479c4b30bd4b51
Lennart Fischer <github@lambdadigamma.com>
2022-09-06 18:51:27 +0200

    preventing form panel to send multiple requests

--- resources/js/Panels/FormPanel.vue
+++ resources/js/Panels/FormPanel.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="bg-white rounded-lg shadow">
-        <form action="#" :method="method">
+        <form :method="method" @submit.prevent="$emit('save')">
             <div class="px-4 py-5 sm:p-6">
                 <slot></slot>
             </div>
             <div class="px-4 py-3 text-right rounded-b-lg bg-gray-50 sm:px-6">
-                <PrimaryButton type="submit" @click.prevent="$emit('save')">
+                <PrimaryButton button-type="submit" :disabled="disabled">
                     {{ actionButton }}
                 </PrimaryButton>
             </div>
@@ -15,20 +15,25 @@
 
 <script>
 import PrimaryButton from "@/Buttons/PrimaryButton.vue";
-import TextInput from "@/Input/TextInput.vue";
+
 export default {
     name: "FormPanel",
-    components: { TextInput, PrimaryButton },
+    components: { PrimaryButton },
     props: {
         actionButton: {
             type: String,
             default: () => "Speichern",
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
         },
         method: {
             type: String,
             default: () => "POST",
         },
     },
+    emits: ["save"],
 };
 </script>
 
